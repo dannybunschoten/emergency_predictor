06cf3f68d8116da1d7d106bd24b1dff13e0f870c
dannybunschoten <danny.bunschoten02@gmail.com>
2024-02-06 11:30:48 +0100

    scaled SVM and random forests perform best

--- script.py
+++ script.py
@@ -2,6 +2,13 @@ import pandas as pd
 from gensim.utils import simple_preprocess
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('train.csv')
@@ -9,7 +16,6 @@ data = pd.read_csv('train.csv')
 data.dropna(subset=['keyword'], inplace=True)
 texts = data['text'].apply(lambda x: simple_preprocess(x))
 preprocessedGenre = data['keyword'].apply(lambda x: simple_preprocess(x))
-
 
 model_genre = Word2Vec(sentences=preprocessedGenre, vector_size=100, window=5, min_count=1, workers=4)
 model_tweet_text = Word2Vec(sentences=texts, vector_size=100, window=5, min_count=1, workers=4)
@@ -26,9 +32,34 @@ X = np.hstack((np.array([document_vector(text, model_tweet_text) for text in tex
 # X = np.array([document_vector(text, model_tweet_text) for text in texts])
 Y = data['target']
 
-tsne = TSNE(n_components=2, random_state=0)
-X_2d = tsne.fit_transform(X)
+# tsne = TSNE(n_components=2, random_state=0)
+# X_2d = tsne.fit_transform(X)
 
-plt.figure(figsize=(15,8))
-plt.scatter(X_2d[:,0], X_2d[:,1], c=Y)
-plt.show()
+# plt.figure(figsize=(15,8))
+# plt.scatter(X_2d[:,0], X_2d[:,1], c=Y)
+# plt.show()
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+models = {
+    "Logistic Regression": LogisticRegression(max_iter=1000),
+    "Random Forest": RandomForestClassifier(),
+    "SVM": SVC(),
+    "KNN": KNeighborsClassifier()
+}
+
+scaled = {
+    "scaled": (X_train_scaled, X_test_scaled),
+    "non-scaled": (X_train, X_test)
+}
+
+for isScaled, data in scaled.items():
+    for name, model in models.items():
+        model.fit(data[0], Y_train)
+        y_pred = model.predict(data[1])
+        accuracy = accuracy_score(Y_test, y_pred)
+        print(f"Model {name} with {isScaled} data has accuracy score: {accuracy:.4f}")
